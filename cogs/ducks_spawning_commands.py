98053c8087c786c27f22736d813c7f3beb848cde
Arthur <arthur@jovart.com>
2020-07-23 00:18:01 +0200

    Add permissions

--- cogs/ducks_spawning_commands.py
+++ cogs/ducks_spawning_commands.py
@@ -3,6 +3,7 @@ import random
 import discord
 from discord.ext import commands
 
+from utils import checks, permissions
 from utils.bot_class import MyBot
 from utils.interaction import get_webhook_if_possible
 
@@ -14,6 +15,7 @@ from utils.ctx_class import MyContext
 
 class DucksSpawningCommands(Cog):
     @commands.group(aliases=["spawn", "spawnduck"])
+    @checks.server_admin_or_permission("ducks.spawn.normal")
     async def coin(self, ctx: MyContext):
         """
         Spawns a duck
@@ -23,12 +25,19 @@ class DucksSpawningCommands(Cog):
             await myduck.spawn()
 
     @coin.command()
-    async def super(self, ctx: MyContext, lives: int):
+    @checks.server_admin_or_permission("ducks.spawn.super")
+    async def super(self, ctx: MyContext, lives: int = None):
         """
         Spawns a super duck
         """
+        can_set_lives = await permissions.has_server_administrator_or_permission(ctx, 'ducks.spawn.super.set_lives')
+        if lives and not can_set_lives:
+            await ctx.send("⚠️ You can't set the lives of a super duck. Ask for the `ducks.spawn.super.set_lives` permission.")
+            lives = None
+
         if not ctx.invoked_subcommand:
-            myduck = SuperDuck(ctx.bot, ctx.channel)
+            myduck = SuperDuck(ctx.bot, ctx.channel, lives=lives)
             await myduck.spawn()
+
 
 setup = DucksSpawningCommands.setup
