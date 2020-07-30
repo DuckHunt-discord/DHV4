1a670793826c1733030bb7f8e39189112b1c1393
Arthur <arthur@jovart.com>
2020-07-30 14:58:22 +0200

    Add DHV2/3 strings to the bot

--- utils/ducks_config.py
+++ utils/ducks_config.py
@@ -10,9 +10,14 @@ DUCKS_IMAGES = {
     'lurk': "https://cdn.discordapp.com/emojis/436542355030409219.png",
     'sip': "https://cdn.discordapp.com/emojis/436548864170786836.png",
     'ghost': "https://cdn.discordapp.com/emojis/660287926902718504.gif",
-    'armored': 'http://worth1000.s3.amazonaws.com/submissions/14291000/14291206_4e56_625x1000.jpg',
+    # 'armored': 'http://worth1000.s3.amazonaws.com/submissions/14291000/14291206_4e56_625x1000.jpg',
     'golden': 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0e43a852-2160-46bd-a515-8e7fbfb35c8d/d3bjtk8-926fdfba-e78f-4167-ae27-4a7011e905eb.jpg/v1/fill/w_900,h_1182,q_75,strp/golden_duck_logo_by_demonicneko-d3bjtk8.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi8wZTQzYTg1Mi0yMTYwLTQ2YmQtYTUxNS04ZTdmYmZiMzVjOGQvZDNianRrOC05MjZmZGZiYS1lNzhmLTQxNjctYWUyNy00YTcwMTFlOTA1ZWIuanBnIiwid2lkdGgiOiI8PTkwMCIsImhlaWdodCI6Ijw9MTE4MiJ9XV19.FUnroTGc7BKGNBvYgSgR0jOVTOEXxNojQA7Nt4dQa_U',
     'plastic': 'https://i1.wp.com/amsterdamduckstore.com/wp-content/uploads/2016/08/Marathon-Rubber-Duck.jpg?resize=400%2C400&ssl=1',
+
+    'ghost_3': 'https://cdn.discordapp.com/attachments/737978240966066177/737978506763567114/ghost_duck3.png',
+    'ghost_2': 'https://cdn.discordapp.com/attachments/737978240966066177/737980172107644969/ghost_duck2.png',
+    'ghost_1': 'https://cdn.discordapp.com/attachments/737978240966066177/737978554754793524/ghost_duck1.png',
+    'armored': 'https://media.discordapp.net/attachments/737978240966066177/738378965294645289/armored_duck.png',
 }
 
 DUCKS_EMOJIS = {
@@ -41,18 +46,45 @@ normal = {
     'emojis': [DUCKS_EMOJIS['walking_duck'], DUCKS_EMOJIS['right_side_duck']],
     'shouts': ["COIN", "COIN", "COIN", "COIN", "COIN", "KWAK", "KWAK", "KWAAK", "KWAAK", "KWAAAK", "KWAAAK", "COUAK", "COUAK", "COUAAK", "COUAAK", "COUAAAK", "COUAAAK", "QUAK",
                "QUAK", "QUAAK", "QUAAK", "QUAAAK", "QUAAAK", "QUACK", "QUACK", "QUAACK", "QUAACK", "QUAAACK", "QUAAACK", "COUAC", "COUAC", "COUAAC", "COUAAC", "COUAAAC", "COUAAAC",
-               "COUACK", "COUACK"],
+               "COUACK", "COUACK", _("*cries*"), _("Hello world"), _("How are you today?"), _("Please don't kill me..."), "<https://youtu.be/r8EBoX9iUw4?t=25>",
+               _("I love you too!"), _("Don't shoot me! I'm a fake duck!"), "<https://youtu.be/_0AAbWqxIs8>",
+               _("**Duck fact**: Some ducks can fly up to 332 miles in a single day!"), _("**Duck fact**: The feathers on a ducks back are waterproof."),
+               _("**Duck fact**: Ducks have three eyelids!"),
+               _("**Duck fact**: Depending on the species, a duck can live between 2 and 12 years. Not the ducks on this game tho..."),
+               _("I am invisible, don't bother killing me"), _("Who is the little ████ here that killed my friend ?"), _("You can't aim for good..."), _("The duck left."),
+               _("The duck right."), _("STOP! It's not what you think!"),
+               _("Killing me will cost you 1562 experience points."),
+               _("I have a secret plan to kill all hunters but I'm already short on breath I don't think I'll be able to explain it all to you before..."),
+               _("[INSERT DUCK NOISE HERE]"), "<https://youtu.be/JZGjyxPc41o>",
+               "https://tenor.com/4nwy.gif", _("It's rabbit season."), _("Got a spare cape?"), _("Which side of a duck has more feathers? The outside."),
+               _("I steal money. I'm a robber ducky."), _("I'm a very special duck."), _("**DuckHunt ProTip®**: use clovers to get more experience."),
+               _("**DuckHunt ProTip®**: use sights to improve accuracy at lower levels."), _("**DuckHunt ProTip®**: use grease to prevent weapon jams"),
+               _("**DuckHunt ProTip®**: use clovers to get more experience."), _("*Slurp*"), _("Hopefully you are all AFK..."),
+               _("**DuckHunt ProTip®**: use infrared detectors to prevent useless shots."),
+               _("I'm just doing my job..."), _("Hey there, DuckHunt quality insurance duck here. How would you rate your experience killing ducks so far ?"),
+               _("YoU hAvE bEeN cOrRuPtEd."), ],
+    'bye_shouts': [_("The duck went away."), _("The duck went to another world."), _("The duck didn't have time for this."),
+            _("The duck left."), _("The duck dissipated in space and time."), _("The duck left out of boredom."),
+            _("The duck doesn't want to be sniped."), _("The duck walked up to the lemonade stand."),
+            _("The duck flew over a disproportionately small gap."),
+            _("The duck chickened out.")],
+    'bye_traces': ["·°'`'°-.,¸¸.·°'`"],
     'usernames': [_("A duck"), _("Mr. Duck"), _("ISO Duck")],
     'avatar_urls': [DUCKS_IMAGES['emoji']],
 }
 
 ghost = {
+    **normal,
     'traces': [],
     'faces': [],
     'emojis': [],
     'shouts': [],
+    'bye_shouts': [_("The ghost never appeared.")],
     'usernames': [_('Invisible Duck'), _('Ghost Duck'), _('Boooo Duck')],
-    'avatar_urls': [DUCKS_IMAGES['ghost']],
+    'avatar_urls': [DUCKS_IMAGES['ghost_1'],
+                    DUCKS_IMAGES['ghost_2'],
+                    DUCKS_IMAGES['ghost_3'],
+                    ],
 }
 
 prof = {**normal,
@@ -83,11 +115,11 @@ plastic = {**normal,
            }
 
 kamikaze = {**normal,
-          'shouts': [_("**DON'T MOVE OR EVERYTHING WILL EXPLODE!**"), _("Gonna kill y'all !")],
-          'usernames': [_("The Kamikaze Duck"), _("Explosive Duck"), _("Boom Duck")],
-          'avatar_urls': [DUCKS_IMAGES['golden']],
-          'emojis': [DUCKS_EMOJIS['golden_duck'], DUCKS_EMOJIS['shiny_duck']]
-          }
+            'shouts': [_("**DON'T MOVE OR EVERYTHING WILL EXPLODE!**"), _("Gonna kill y'all !")],
+            'usernames': [_("The Kamikaze Duck"), _("Explosive Duck"), _("Boom Duck")],
+            'avatar_urls': [DUCKS_IMAGES['golden']],
+            'emojis': [DUCKS_EMOJIS['golden_duck'], DUCKS_EMOJIS['shiny_duck']]
+            }
 
 mechanical = {
     **normal,
