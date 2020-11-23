b55aff31bdcafd7c5bd5acccd0b74a176082b3c3
Arthur <arthur@jovart.com>
2020-11-23 12:49:38 +0100

    Add freetime command

--- cogs/simple_commands.py
+++ cogs/simple_commands.py
@@ -1,10 +1,16 @@
+import datetime
 import time
 
+from babel.dates import format_timedelta
 from discord.ext import commands
 
 from utils.cog_class import Cog
 from utils.ctx_class import MyContext
 
+SECOND = 1
+MINUTE = 60 * SECOND
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
 
 class SimpleCommands(Cog):
     @commands.command()
@@ -38,5 +44,24 @@ class SimpleCommands(Cog):
         """
 
         await ctx.send(f"<https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=204856385>")
+
+
+    @commands.command()
+    async def freetime(self, ctx: MyContext):
+        """
+        Get the time when you'll get free magazines and your weapon back from the police
+        """
+
+        _ = await ctx.get_translate_function()
+        language_code = await ctx.get_language_code()
+
+        now = int(time.time())
+        SECONDS_SPENT_TODAY = now % 86400
+        SECONDS_LEFT_TODAY = 86400 - SECONDS_SPENT_TODAY
+        time_delta = datetime.timedelta(seconds=SECONDS_LEFT_TODAY)
+
+        formatted_delta = format_timedelta(time_delta, add_direction=True, locale=language_code)
+
+        await ctx.send(_("You'll get back your weapon and magazines {formatted_delta}", formatted_delta=formatted_delta))
 
 setup = SimpleCommands.setup
