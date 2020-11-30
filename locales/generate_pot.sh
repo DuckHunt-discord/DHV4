0bc83b4fe0d87d6a9a4fab5159ed691943ca286f
Arthur Jovart <arthur@jovart.com>
2020-11-30 22:23:42 +0100

    Add docstrings to translation files

--- locales/generate_pot.sh
+++ locales/generate_pot.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # pybabel extract -o messages.pot ../
-xgettext --language=Python --add-comments=TRANSLATORS: --force-po -o ./messages.pot -vv ../**/*.py
+
+# Just extract docstrings
+python3 ./pygettext.py --docstrings --no-default-keywords --output=./messages.pot --verbose ../
+
+# Add messages
+xgettext --language=Python --add-comments=TRANSLATORS: --force-po -o ./messages.pot --join-existing -vv ../**/*.py
 
 # pybabel init -l en -i ./messages.pot -d ./
 # pybabel compile -d ./
