3f9a8bd6ed490b294463ade3fecc90b7bbdd04a9
Arthur Jovart <arthur@jovart.com>
2020-12-13 23:24:07 +0100

    Batter .pot file

--- src/locales/generate_pot.sh
+++ src/locales/generate_pot.sh
@@ -2,10 +2,12 @@
 # pybabel extract -o messages.pot ../
 
 # Just extract docstrings
-python3 ./pygettext.py --docstrings --no-default-keywords --output=./messages.pot --verbose ../
+python3 ./pygettext.py -X EXCLUDES --docstrings --no-default-keywords --output=./messages.pot --verbose ../
 
 # Add messages
-xgettext --language=Python --add-comments=TRANSLATORS: --force-po -o ./messages.pot --join-existing -vv ../**/*.py
+xgettext --language=Python --add-comments=TRANSLATORS: --force-po -o ./messages.pot --join-existing -vvv ../cogs/*.py
+xgettext --language=Python --add-comments=TRANSLATORS: --force-po -o ./messages.pot --join-existing -vvv ../utils/*.py
+xgettext --language=Python --add-comments=TRANSLATORS: --force-po -o ./messages.pot --join-existing -vvv ../main.py
 
 # pybabel init -l en -i ./messages.pot -d ./
 # pybabel compile -d ./
