bca3503eb2959037e09a841e07b4551085ae79a3
Arthur Jovart <arthur@jovart.com>
2021-01-05 15:05:45 +0100

    Coat last for a day

--- src/cogs/shopping_commands.py
+++ src/cogs/shopping_commands.py
@@ -642,7 +642,7 @@ class ShoppingCommands(Cog):
 
         await db_hunter.edit_experience_with_levelups(ctx, -ITEM_COST)
         db_hunter.active_powerups["wet"] = 0
-        db_hunter.active_powerups["coat"] = int(time.time()) + HOUR
+        db_hunter.active_powerups["coat"] = int(time.time()) + DAY
 
         db_hunter.bought_items['coat'] += 1
 
