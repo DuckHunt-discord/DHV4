863d7e1fe03cf0d46f4926bdf570b00cb9dff2cf
Arthur Jovart <arthur@jovart.com>
2021-01-05 12:42:55 +0100

    Update docker_run to sleep before starting

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
 python /generate_config_from_env.py /docker_config.toml /bot/config.toml
+
+echo "Waiting a few seconds before starting"
+sleep 10
+echo "Starting..."
+
 exec python /bot/main.py
