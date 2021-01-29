5eb6a5a1c06a81b32f05117814aa2e45354b57d3
Arthur Jovart <arthur@jovart.com>
2021-01-29 11:52:43 +0100

    Add random ducks generator

--- src/cogs/support_server_commands.py
+++ src/cogs/support_server_commands.py
@@ -4,6 +4,8 @@ Some example of commands that can be used only in the bot support server.
 
 import datetime
 import time
+from functools import partial
+from io import BytesIO
 
 import discord
 import pytz
@@ -17,6 +19,7 @@ from utils.interaction import purge_channel_messages, EmbedCounterPaginator
 from babel import dates
 
 from utils.models import AccessLevel, get_from_db
+from utils.random_ducks import create_random_duck
 
 
 class SupportServerCommands(Cog):
@@ -140,6 +143,35 @@ class SupportServerCommands(Cog):
                                                      ))
         await menu.start(ctx)
 
+    @staticmethod
+    def get_random_duck_bytes(with_background=True):
+        image = create_random_duck(with_background)
+
+        # prepare the stream to save this image into
+        buffer = BytesIO()
+
+        # save into the stream, using png format.
+        image.save(buffer, "png")
+        buffer.seek(0)
+        return buffer
+
+    @commands.command(aliases=["rd"])
+    async def random_duck(self, ctx: MyContext, with_background=True):
+        """
+        Shows a random duck image, courtesy of Globloxmen assets.
+        This is mostly used to debug the random duck functions,
+        and uses the same pipeline as for the random duck avatars.
+
+        You can specify if you want a background or not for your random duck, and it defaults to yes.
+        """
+        async with ctx.typing():
+            _ = await ctx.get_translate_function()
+            fn = partial(self.get_random_duck_bytes, with_background)
+
+            buffer = await self.bot.loop.run_in_executor(None, fn)
+            file = discord.File(filename="random_duck.png", fp=buffer)
+
+            await ctx.send(file=file)
 
     @commands.group(aliases=["bot_administration", "emergencies"])
     @checks.needs_access_level(AccessLevel.BOT_MODERATOR)
---
+++ src/utils/random_ducks.py
@@ -0,0 +1,34 @@
+import pathlib
+import random
+from PIL import Image
+from utils.config import load_config
+
+config = load_config()['random_ducks']
+random_ducks_assets = pathlib.Path(config['assets_path'])
+
+assert random_ducks_assets.exists()
+
+
+def get_random_asset_from_directory(directory_name) -> Image.Image:
+    directory = random_ducks_assets / directory_name
+
+    assert directory.exists() and directory.is_dir()
+
+    file_path = random.choice(list(directory.glob("*.png")))
+
+    return Image.open(file_path)
+
+
+def create_random_duck(with_background=True) -> Image.Image:
+    if with_background:
+        image = get_random_asset_from_directory('Background')
+        image.alpha_composite(get_random_asset_from_directory(config['composite_order'][0]))
+    else:
+        image = get_random_asset_from_directory(config['composite_order'][0])
+
+    for directory_name in config['composite_order'][1:]:
+        image.alpha_composite(get_random_asset_from_directory(directory_name))
+
+    return image
+
+
