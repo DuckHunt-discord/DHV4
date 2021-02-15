1bcf711e33e57f10213221d3eb4feed366dda894
Arthur Jovart <arthur@jovart.com>
2021-02-15 15:42:15 +0100

    Add custom statcord values

--- src/cogs/bots_list_and_voting.py
+++ src/cogs/bots_list_and_voting.py
@@ -11,18 +11,26 @@ from typing import Tuple, List
 from utils import checks
 from cogs.inventory_commands import INV_COMMON_ITEMS
 from utils.ctx_class import MyContext
-from utils.models import DiscordUser, get_from_db, BotList, Vote
+from utils.models import DiscordUser, get_from_db, BotList, Vote, Player
 
 
 class BotsListVoting(Cog):
     def __init__(self, bot, *args, **kwargs):
         super().__init__(bot, *args, **kwargs)
-        self.statcord_api = statcord.Client(self.bot, self.config()['statcord_token'])
+        self.statcord_api = statcord.Client(self.bot, self.config()['statcord_token'],
+                                            custom1=self.statcord_custom_value_ducks_spawned,
+                                            custom2=self.statcord_custom_value_players_count)
         self.statcord_api.start_loop()
 
     @Cog.listener()
     async def on_command(self, ctx):
         self.statcord_api.command_run(ctx)
+
+    async def statcord_custom_value_ducks_spawned(self):
+        return sum(len(v) for v in self.bot.ducks_spawned.values())
+
+    async def statcord_custom_value_players_count(self):
+        return await Player.all().count()
 
     async def get_bot_list(self) -> List[BotList]:
         return await BotList.all()
