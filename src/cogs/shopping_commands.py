9962d658027a010026a4ade8c7809041ecb9da50
Arthur Jovart <arthur@jovart.com>
2021-02-19 15:29:24 +0100

    Fix coat buying message.

--- src/cogs/shopping_commands.py
+++ src/cogs/shopping_commands.py
@@ -698,16 +698,16 @@ class ShoppingCommands(Cog):
         db_hunter.bought_items['coat'] += 1
 
         await db_hunter.save()
-        await ctx.reply(_("💸 You bought a new, {color} coloured coat. You don't look very good, "
-                          "but you are very much protected from water. "
-                          "The tag on the coat says the follwoing :\n"
+        await ctx.reply(_("💸 You bought a new, **{color}** coat. \nYou don't look very good, "
+                          "but you are very much protected from water.\n\n"
+                          "The tag on the coat says the following :\n"
                           "> 100% Polyester, machine washable\n"
                           "> {hint}\n"
                           "> Made in a CACAD factory also producing homing bullets\n\n"
                           "[Bought: -{ITEM_COST} exp, total {db_hunter.experience} exp]",
                           db_hunter=db_hunter,
                           ITEM_COST=ITEM_COST,
-                          color=_(color.value[0]),
+                          color=_(color.value[0]).lower(),
                           hint=_(color.value[1])
                           ))
 
