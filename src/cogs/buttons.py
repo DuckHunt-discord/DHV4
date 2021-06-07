9a81de07592cb293ba7accbe0696fd6c1d8d606c
Arthur Jovart <arthur@jovart.com>
2021-06-07 23:50:51 +0200

    Buttons are still a bit wonky.

--- src/cogs/buttons.py
+++ src/cogs/buttons.py
@@ -1,8 +1,10 @@
 import time
+import typing
 
 import discord
 from discord.ext import commands
 
+from utils.bot_class import MyBot
 from utils.cog_class import Cog
 from utils.ctx_class import MyContext
 
@@ -13,7 +15,8 @@ DAY = 24 * HOUR
 
 
 class CounterView(discord.ui.View):
-    @discord.ui.button(label='0', style=discord.ButtonStyle.red)
+
+    @discord.ui.button(label='0', style=discord.ButtonStyle.red, custom_id='count_button')
     async def count(self, button: discord.ui.Button, interaction: discord.Interaction):
         number = int(button.label)
         if number + 1 >= 5:
@@ -28,7 +31,8 @@ class Buttons(Cog):
         """
         Click on the button to count.
         """
-        await ctx.send(content="Click me to count!", view=CounterView())
+        m = await ctx.send(content="Click me to count!", view=CounterView(timeout=None))
+        # self.bot.add_view(CounterView(timeout=None), message_id=m.id)
 
 
 setup = Buttons.setup
