835603b9dc4763bada844daf5a970d921ee6e030
Arthur Jovart <arthur@jovart.com>
2021-06-07 22:51:40 +0200

    Let's see how buttons work.

---
+++ src/cogs/buttons.py
@@ -0,0 +1,34 @@
+import time
+
+import discord
+from discord.ext import commands
+
+from utils.cog_class import Cog
+from utils.ctx_class import MyContext
+
+SECOND = 1
+MINUTE = 60 * SECOND
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
+
+
+class CounterView(discord.ui.View):
+    @discord.ui.button(label='0', style=discord.ButtonStyle.red)
+    async def count(self, button: discord.ui.Button, interaction: discord.Interaction):
+        number = int(button.label)
+        if number + 1 >= 5:
+            button.style = discord.ButtonStyle.green
+        button.label = str(number + 1)
+        await interaction.message.edit(view=self)
+
+
+class Buttons(Cog):
+    @commands.command()
+    async def count(self, ctx: MyContext):
+        """
+        Click on the button to count.
+        """
+        await ctx.send(view=CounterView())
+
+
+setup = Buttons.setup
