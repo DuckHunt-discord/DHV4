9fd61bbc9aaa98c146546f0a0c8f9c71071f60ef
Arthur Jovart <arthur@jovart.com>
2021-07-04 18:14:10 +0200

    Add new command

--- src/cogs/buttons.py
+++ src/cogs/buttons.py
@@ -7,7 +7,7 @@ from utils.cog_class import Cog
 from utils.ctx_class import MyContext
 from utils.models import AccessLevel
 
-from utils.views import init_all_persistant_command_views, CommandView
+from utils.views import init_all_persistant_command_views, CommandView, nitro_prank
 
 SECOND = 1
 MINUTE = 60 * SECOND
@@ -40,6 +40,12 @@ class Buttons(Cog):
     @needs_access_level(AccessLevel.BOT_MODERATOR)
     async def create_command_button(self, ctx: MyContext, *, command_name: str):
         await CommandView(self.bot, command_to_be_ran=command_name, label=command_name, style=ButtonStyle.blurple, command_can_run=True).send(ctx)
+
+    @commands.command()
+    @needs_access_level(AccessLevel.MODERATOR)
+    async def get_nitro_button(self, ctx: MyContext):
+        await ctx.message.delete(delay=0)
+        await nitro_prank(ctx)
 
 
 setup = Buttons.setup
