9e34304a906ae7b04fa06850f524f5b276fcbda4
0/0 <zero@imaskeleton.me>
2021-07-12 11:05:58 -0600

    remove nested if block

--- src/cogs/ducks_hunting_commands.py
+++ src/cogs/ducks_hunting_commands.py
@@ -379,11 +379,10 @@ class DucksHuntingCommands(Cog):
             db_hunter.shooting_stats['shots_with_duck'] += 1
             duck.db_target_lock_by = db_hunter  # Since we have unsaved data
             result = await duck.shoot(args)
-            if result is not None:
-                if not result and db_hunter.is_powerup_active('detector'):
-                    db_hunter.bullets += 1
-                    db_hunter.shooting_stats['bullets_used'] -= 1
-                    await db_hunter.save()
+            if result is False and db_hunter.is_powerup_active('detector'):
+                db_hunter.bullets += 1
+                db_hunter.shooting_stats['bullets_used'] -= 1
+                await db_hunter.save()
         elif db_hunter.is_powerup_active('detector'):
             db_hunter.active_powerups['detector'] -= 1
             db_hunter.shooting_stats['shots_stopped_by_detector'] += 1
