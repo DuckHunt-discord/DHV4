d57affe5a8f7e2ee39361d9816b52edc86cbccd1
Arthur Jovart <arthur@jovart.com>
2021-07-21 21:01:39 +0200

    Add gamepad view

--- src/cogs/buttons.py
+++ src/cogs/buttons.py
@@ -1,3 +1,4 @@
+import discord
 from discord import ButtonStyle
 from discord.ext import commands
 
@@ -7,12 +8,25 @@ from utils.cog_class import Cog
 from utils.ctx_class import MyContext
 from utils.models import AccessLevel
 
-from utils.views import init_all_persistant_command_views, CommandView, nitro_prank
+from utils.views import init_all_persistant_command_views, CommandView, nitro_prank, View, CommandButton
 
 SECOND = 1
 MINUTE = 60 * SECOND
 HOUR = 60 * MINUTE
 DAY = 24 * HOUR
+
+
+class GamepadView(View):
+    def __init__(self, bot: MyBot):
+        super().__init__(bot)
+
+        bang_command = bot.get_command("bang")
+        hug_command = bot.get_command("hug")
+        reload_command = bot.get_command("reload")
+
+        self.add_item(CommandButton(bot, bang_command, [], {'target': None}, custom_id="gamepad_bang", row=0, label='🔫 Bang', style=discord.ButtonStyle.red))
+        self.add_item(CommandButton(bot, hug_command, [], {'target': None}, custom_id="gamepad_hug", row=0, label='🤗 Hug', style=discord.ButtonStyle.green))
+        self.add_item(CommandButton(bot, reload_command, [], {}, custom_id="gamepad_reload", row=1, label='♻️ Reload', style=discord.ButtonStyle.blurple))
 
 
 class Buttons(Cog):
@@ -32,6 +46,7 @@ class Buttons(Cog):
             # we don't have one.
             self.bot.logger.debug("Loading persistant views...")
             await init_all_persistant_command_views(self.bot)
+            self.bot.add_view(GamepadView(self.bot))
             self.bot.logger.info("Command persistant views loaded")
 
             self.persistent_views_added = True
@@ -46,6 +61,11 @@ class Buttons(Cog):
     async def get_nitro_button(self, ctx: MyContext):
         await ctx.message.delete(delay=0)
         await nitro_prank(ctx)
+
+    @commands.command(hidden=True)
+    @needs_access_level(AccessLevel.BOT_OWNER)
+    async def gamepad(self, ctx: MyContext):
+        await GamepadView(self.bot).send(ctx)
 
 
 setup = Buttons.setup
