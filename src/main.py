8f4cb4de630828f7b4197f6e1789d35c50f635e0
Arthur <site-github@api-d.com>
2022-03-31 10:07:03 +0200

    Did I tell you how much I hate this lib?

--- src/main.py
+++ src/main.py
@@ -12,19 +12,19 @@ config = load_config()
 
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.Intents
 intents = discord.Intents.none()
-intents.guilds       = True
-intents.messages     = True
-intents.reactions    = True
+intents.guilds = True
+intents.messages = True
+intents.reactions = True
 
-intents.presences    = False  # Privileged
-intents.members      = False  # Privileged
-intents.bans         = False
-intents.emojis       = False
+intents.presences = False  # Privileged
+intents.members = False  # Privileged
+intents.bans = False
+intents.emojis = False
 intents.integrations = False
-intents.webhooks     = False
-intents.invites      = False
+intents.webhooks = False
+intents.invites = False
 intents.voice_states = False
-intents.typing       = False
+intents.typing = False
 
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.AllowedMentions
 allowed_mentions = discord.AllowedMentions(
@@ -41,11 +41,17 @@ bot = MyBot(
     chunk_guilds_at_startup=False,
 )
 
-for cog_name in config["cogs"]["cogs_to_load"]:
-    try:
-        bot.load_extension(cog_name)
-        bot.logger.debug(f"> {cog_name} loaded!")
-    except Exception as e:
-        bot.logger.exception('> Failed to load extension {}\n{}: {}'.format(cog_name, type(e).__name__, e))
 
-bot.run(config['auth']['discord']['token'])
+async def main():
+    for cog_name in config["cogs"]["cogs_to_load"]:
+        try:
+            await bot.load_extension(cog_name)
+            bot.logger.debug(f"> {cog_name} loaded!")
+        except Exception as e:
+            bot.logger.exception('> Failed to load extension {}\n{}: {}'.format(cog_name, type(e).__name__, e))
+
+    async with bot:
+        await bot.start(config['auth']['discord']['token'])
+
+
+asyncio.run(main())
