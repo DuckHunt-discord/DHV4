9b55d2676d40cf0733e165f13a6465c4d71ecfde
Arthur <site-github@api-d.com>
2022-09-22 13:44:22 +0200

    Tentative fix for translators

--- src/cogs/simple_commands.py
+++ src/cogs/simple_commands.py
@@ -47,7 +47,7 @@ class TranslatorsMenusSource(menus.ListPageSource):
                               "Contact Eyesofcreeper#0001 on the support server (`{ctx.prefix}invite`). Thanks!")
 
         offset = menu.current_page * self.per_page
-        for i, item in enumerate(entries, start=offset):
+        for i, item in enumerate(entries.items(), start=offset):
             locale, data = item
 
             parsed_translators = map(lambda user: f"{user.name}#{user.discriminator}", data['users'])
