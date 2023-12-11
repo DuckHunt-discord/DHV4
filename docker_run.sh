8e46b5982754c143555d25b4070afdd8154c4c7d
Arthur <site-github@api-d.com>
2023-12-11 15:04:42 +0100

    Lets go

--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,11 @@
 
 python /duckhunt/generate_config_from_env.py /duckhunt/docker_config.toml /duckhunt/src/config.toml
 
+echo "Setting current branch..."
+git pull origin master || true
+
+git status
+
 echo "Waiting a few seconds before starting..."
 sleep 5
 echo "Starting..."
